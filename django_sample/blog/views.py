bce2207e33c4c8cb22d42348a3b1342ecf88a0f7
mingkim <ming3772@gmail.com>
2015-11-07 12:05:10 +0900

    ADD - blog url

--- django_sample/blog/views.py
+++ django_sample/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context = {}
+    template = 'index.html'
+
+    # code
+
+    return render(request, template, context)
