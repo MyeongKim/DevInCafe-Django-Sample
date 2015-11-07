5c854fb39fe8bbc28862a07b61867962fe5cf60c
mingkim <ming3772@gmail.com>
2015-11-07 11:42:40 +0900

    UPDATE - setting admin, static

--- django_sample/blog/models.py
+++ django_sample/blog/models.py
@@ -6,3 +6,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User)
     nickname = models.CharField(max_length=20)
     removed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "[{}] {}".format(self.user, self.nickname)
