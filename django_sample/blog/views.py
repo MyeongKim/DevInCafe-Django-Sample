2e5445500881eb0547f3c46bd69e69e66b26fe73
mingkim <ming3772@gmail.com>
2015-11-28 12:45:29 +0900

    UPDATE - add post

--- django_sample/blog/views.py
+++ django_sample/blog/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from blog.forms import PostForm
 from blog.models import Post
 
 
 def index(request):
-    title = 'DevInCafe Sample | Blog'
+    title = 'DevInCafe | Blog'
 
-    post_list = Post.objects.all()
+    post_list = Post.objects.filter(removed=False)
 
     context = {
-        'WEB_TITLE' : title,
-        'post_list' : post_list,
+        'WEB_TITLE': title,
+        'e_post_list': enumerate(post_list),
     }
     return render(request, 'blog/pages/index.html', context)
+
+
+def post_create(request):
+    title = 'DevInCafe | NEW'
+    context = {
+        'WEB_TITLE': title,
+    }
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:index')
+
+    else:
+        form = PostForm()
+
+    context.update({'form': form})
+
+    return render(request, 'blog/pages/add_post.html', context)
